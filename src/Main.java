b938fcb47431c18ac8185e9da5378fefafca9afc
Kira9112 <kira91_911@mail.ru>
2021-06-17 22:58:32 +0300

    Modified Java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int TicketPrice = 10268;
-        int OneMileInRub = 20;
-        int Mile = TicketPrice / OneMileInRub;
-        System.out.println(Mile);
+        int ticketPrice = 10268;
+        int oneMileInRub = 20;
+        int mile = ticketPrice / oneMileInRub;
+        System.out.println(mile);
     }
 }
